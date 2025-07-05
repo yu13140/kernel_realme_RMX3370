21004edde8d06ceac21e9132d5aec63fde9d638a
yu13140 <whmyc801@gmail.com>
2025-07-06 01:15:44 +0800

    sched.h : patch susfs 1.5.8

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1537,6 +1537,9 @@ struct task_struct {
 	 * New fields for task_struct should be added above here, so that
 	 * they are included in the randomized portion of task_struct.
 	 */
+#ifdef CONFIG_KSU_SUSFS
+ 	u64 susfs_task_state;
+ #endif
 	randomized_struct_fields_end
 
 #ifdef CONFIG_OPLUS_FEATURE_FUSE_FS_SHORTCIRCUIT
